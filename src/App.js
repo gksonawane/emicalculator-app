5c811887ab17dfc6044cb3b3e1061c422eaf7e5d
Gauravkumar Sonawane <gauravsonawane764@gmail.com>
2024-02-20 09:02:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import Emicalculate from './components/Emicalculate';
-// import GraphPie from './components/GraphPie';
-
-
 
 function App() {
-  
-  
+
   return (
     <>
     <Emicalculate/>
-    {/* <GraphPie/> */}
     </>
   );
 }
